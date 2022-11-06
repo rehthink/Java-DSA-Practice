1a84777bc6e3b18322a27cf4e7e30a346a299026
Rehan Ahmed Khan <rehnk007@gmail.com>
2022-11-06 20:35:12 +0530

    Maths Problem

---
+++ src/MathsProblem/NewtonRaphsonMethod.java
@@ -0,0 +1,25 @@
+package MathsProblem;
+
+public class NewtonRaphsonMethod {
+    public static void main(String[] args) {
+
+        double n = 30.0;
+        System.out.println(sqrt(n));
+    }
+
+    static double sqrt(double n) {
+        double x = n;
+        double root;
+
+        while (true) {
+            root = 0.5 * (x + (n / x));
+            if (Math.abs(root - x) < 0.5) {
+                break;
+            }
+
+            x = root;
+        }
+
+        return root;
+    }
+}
---
+++ src/MathsProblem/SieveOfEratosthenes.java
@@ -0,0 +1,40 @@
+package MathsProblem;
+
+public class SieveOfEratosthenes {
+
+    /*
+    * Given a number n, print all primes smaller
+    * than or equal to n. It is also given that
+    * n is a small number.
+    *
+    * Space Complexity : O(n)
+    * Time Complexity : O( N* log (log n) )
+    *
+     * */
+
+    public static void main(String[] args) {
+
+        int n = 40;
+        boolean[] primes = new boolean[n+1];
+        sieve(n, primes);
+
+    }
+
+    static void sieve(int n, boolean[] primes) {
+        for (int i = 2; i * i <= n ; i++) {
+
+            if (!primes[i]) {
+                for (int j = i*2; j <=n ; j+=i) {
+                    primes[j] = true;
+                }
+            }
+
+        }
+
+        for (int i = 2; i <=n ; i++) {
+            if (!primes[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
